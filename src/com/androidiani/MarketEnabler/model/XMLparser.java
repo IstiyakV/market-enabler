70dd374874a8638d92553e762b2be4fdac440974
michel.racic <michel.racic@a70d69da-39a2-11de-8ef0-0f53c1b7e458>
2009-05-09 11:14:29 +0000

    added xmlparser stub

---
+++ src/com/androidiani/MarketEnabler/model/XMLparser.java
@@ -0,0 +1,10 @@
+package com.androidiani.MarketEnabler.model;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class XMLparser extends DefaultHandler {
+	public XMLparser() {
+		super();
+	}
+}
