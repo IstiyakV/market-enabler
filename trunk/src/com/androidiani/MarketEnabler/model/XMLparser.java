7b1961c3c2ff6bf51243a3900197ee06b821ea4d
michel.racic <michel.racic@a70d69da-39a2-11de-8ef0-0f53c1b7e458>
2009-05-09 11:25:29 +0000

    xml parser implemented basic methods

--- trunk/src/com/androidiani/MarketEnabler/model/XMLparser.java
+++ trunk/src/com/androidiani/MarketEnabler/model/XMLparser.java
@@ -1,5 +1,6 @@
 package com.androidiani.MarketEnabler.model;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 
@@ -7,4 +8,56 @@ public class XMLparser extends DefaultHandler {
 	public XMLparser() {
 		super();
 	}
+	
+	public void startDocument() {
+		System.out.println("Start document");
+	}
+
+	public void endDocument() {
+		System.out.println("End document");
+	}
+
+	public void startElement(String uri, String name, String qName,
+			Attributes atts) {
+		if ("".equals(uri))
+			System.out.println("Start element: " + qName);
+		else
+			System.out.println("Start element: {" + uri + "}" + name);
+	}
+
+	public void endElement(String uri, String name, String qName) {
+		if ("".equals(uri))
+			System.out.println("End element: " + qName);
+		else
+			System.out.println("End element:   {" + uri + "}" + name);
+	}
+	
+	public void characters(char ch[], int start, int length) {
+		System.out.print("Characters:    \"");
+		for (int i = start; i < start + length; i++) {
+			switch (ch[i]) {
+			case '\\':
+				System.out.print("\\\\");
+				break;
+			case '"':
+				System.out.print("\\\"");
+				break;
+			case '\n':
+				System.out.print("\\n");
+				break;
+			case '\r':
+				System.out.print("\\r");
+				break;
+			case '\t':
+				System.out.print("\\t");
+				break;
+			default:
+				System.out.print(ch[i]);
+				break;
+			}
+		}
+		System.out.print("\"\n");
+	}
+
+
 }
