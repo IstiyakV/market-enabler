420f1e4670ca5148c2c5c0fef9efa6e4d45426d0
michel.racic <michel.racic@a70d69da-39a2-11de-8ef0-0f53c1b7e458>
2009-05-09 11:48:41 +0000

    added startelemnt code to sax handler

--- trunk/src/com/androidiani/MarketEnabler/model/XML2ObjectHandler.java
+++ trunk/src/com/androidiani/MarketEnabler/model/XML2ObjectHandler.java
@@ -5,31 +5,66 @@ import org.xml.sax.helpers.DefaultHandler;
 
 
 public class XML2ObjectHandler extends DefaultHandler {
+	private int gsmSimOperatorNumeric;
+	private int gsmOperatorNumeric;
+	private String gsmSimOperatorIsoCountry;
+	private String gsmOperatorIsoCountry;
+	private String gsmSimOperatorAlpha;
+	private String gsmOperatorAlpha;
+
+	private boolean inSimNum = false;
+	private boolean inOpNum = false;
+	private boolean inSimIso = false;
+	private boolean inOpIso = false;
+	private boolean inSimAlpha = false;
+	private boolean inOpAlpha = false;
+	private boolean inHash = false;
+	private boolean inConfig = false;
+	
 	public XML2ObjectHandler() {
 		super();
 	}
 	
-	public void startDocument() {
-		// Create empty Provider
-	}
-
-	public void endDocument() {
-		System.out.println("End document");
-	}
-
 	public void startElement(String uri, String name, String qName,
 			Attributes atts) {
-		if ("".equals(uri))
-			System.out.println("Start element: " + qName);
-		else
-			System.out.println("Start element: {" + uri + "}" + name);
+		if (name.trim().equals("configuration")) {
+			this.inConfig = true;
+		} else if (name.trim().equals("gsmSimOperatorNumeric")) {
+			this.inSimNum = true;
+		} else if (name.trim().equals("gsmSimOperatorIso-Country")) {
+			this.inSimIso = true;
+		} else if (name.trim().equals("gsmSimOperatorAlpha")) {
+			this.inSimAlpha = true;
+		} else if (name.trim().equals("gsmOperatorNumeric")) {
+			this.inOpNum = true;
+		} else if (name.trim().equals("gsmOperatorIso-Country")) {
+			this.inOpIso = true;
+		} else if (name.trim().equals("gsmOperatorAlpha")) {
+			this.inOpAlpha = true;
+		} else if (name.trim().equals("SettingsHash")) {
+			this.inHash = true;
+		} 
 	}
 
 	public void endElement(String uri, String name, String qName) {
-		if ("".equals(uri))
-			System.out.println("End element: " + qName);
-		else
-			System.out.println("End element:   {" + uri + "}" + name);
+		if (name.trim().equals("configuration")) {
+			this.inConfig = false;
+		} else if (name.trim().equals("gsmSimOperatorNumeric")) {
+			this.inSimNum = false;
+		} else if (name.trim().equals("gsmSimOperatorIso-Country")) {
+			this.inSimIso = false;
+		} else if (name.trim().equals("gsmSimOperatorAlpha")) {
+			this.inSimAlpha = false;
+		} else if (name.trim().equals("gsmOperatorNumeric")) {
+			this.inOpNum = false;
+		} else if (name.trim().equals("gsmOperatorIso-Country")) {
+			this.inOpIso = false;
+		} else if (name.trim().equals("gsmOperatorAlpha")) {
+			this.inOpAlpha = false;
+		} else if (name.trim().equals("SettingsHash")) {
+			this.inHash = false;
+		} 
+		
 	}
 	
 	public void characters(char ch[], int start, int length) {
