0418f79c711034866ccad3916bf5350a9cfce733
michel.racic <michel.racic@gmail.com>
2009-09-05 13:09:25 +0000

    fixed T-Mobile value in predefined list

--- src/com/androidiani/MarketEnabler/presenter/ListPresenter.java
+++ src/com/androidiani/MarketEnabler/presenter/ListPresenter.java
@@ -10,12 +10,12 @@ import android.util.Log;
 import com.androidiani.MarketEnabler.model.ProviderConfig;
 
 public class ListPresenter implements Runnable {
-	private IListView view;
+	private final IListView view;
 	private static Handler handler;
 	private static String[] writePropCommand;
 	private ProgressDialog pd;
 	private ArrayList<ProviderConfig> list;
-	
+
 	public ProgressDialog getPd() {
 		return pd;
 	}
@@ -25,22 +25,22 @@ public class ListPresenter implements Runnable {
 		handler = view.getHandler();
 		createDefaultList();
 	}
-	
+
 	public ArrayList<ProviderConfig> createDefaultList() {
 		list = new ArrayList<ProviderConfig>();
 		list.add(new ProviderConfig(22802, 22802, "ch", "ch", "sunrise",
-				"sunrise"));
+		"sunrise"));
 		list.add(new ProviderConfig(26207, 26207, "de", "de", "o2 - de",
-				"o2 - de"));
+		"o2 - de"));
 		list
-				.add(new ProviderConfig(26203, 26203, "de", "de", "simyo",
-						"E-Plus"));
-		list.add(new ProviderConfig(31026, 31026, "us", "us", "T-Mobile",
-				"T-Mobile"));
-		
+		.add(new ProviderConfig(26203, 26203, "de", "de", "simyo",
+		"E-Plus"));
+		list.add(new ProviderConfig(310260, 310260, "us", "us", "T-Mobile",
+		"T-Mobile"));
+
 		return list;
 	}
-	
+
 	private static void setCommand(String[] cmd) {
 		writePropCommand = cmd;
 	}
@@ -56,22 +56,22 @@ public class ListPresenter implements Runnable {
 		// getting values from view and creating shell command
 		String[] writePropCommand = {
 				"setprop gsm.sim.operator.numeric "
-						+ settings.getGsmSimOperatorNumeric(),
+				+ settings.getGsmSimOperatorNumeric(),
 				"setprop gsm.operator.numeric "
-						+ settings.getGsmOperatorNumeric(),
+				+ settings.getGsmOperatorNumeric(),
 				"setprop gsm.sim.operator.iso-country "
-						+ settings.getGsmSimOperatorIsoCountry(),
+				+ settings.getGsmSimOperatorIsoCountry(),
 				"setprop gsm.operator.iso-country "
-						+ settings.getGsmOperatorIsoCountry(),
+				+ settings.getGsmOperatorIsoCountry(),
 				"setprop gsm.operator.alpha \""
-						+ settings.getGsmOperatorAlpha() + "\"",
+				+ settings.getGsmOperatorAlpha() + "\"",
 				"setprop gsm.sim.operator.alpha \""
-						+ settings.getGsmSimOperatorAlpha() + "\"",
+				+ settings.getGsmSimOperatorAlpha() + "\"",
 				"kill $(ps | grep vending | tr -s  ' ' | cut -d ' ' -f2)",
-				"rm -rf /data/data/com.android.vending/cache/*" };
-		
+		"rm -rf /data/data/com.android.vending/cache/*" };
+
 		setCommand(writePropCommand);
-		
+
 		Log.i("MarketEnabler", "dropping shell commands for list values");
 		// pd = new ProgressDialog(view.getStartup());
 		pd = new ProgressDialog((Context) view);
@@ -82,7 +82,7 @@ public class ListPresenter implements Runnable {
 		pd.setMessage("We Are Faking: " + settings.getGsmSimOperatorAlpha()
 				+ "(" + settings.getGsmOperatorAlpha() + ")");
 		pd.show();
-		
+
 		Thread thread = new Thread(this);
 		thread.start();
 
