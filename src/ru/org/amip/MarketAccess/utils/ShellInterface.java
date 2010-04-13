be8ead173267c978eecd478770b3581a17df0a9b
sbaranov <sbaranov@a70d69da-39a2-11de-8ef0-0f53c1b7e458>
2010-04-13 11:44:53 +0000

    refactor process execution so that each command runs in its own shell, now we don't rely on sleep to ensure that shell command is complete before executing java command depending on the result of the shell command

--- src/ru/org/amip/MarketAccess/utils/ShellInterface.java
+++ src/ru/org/amip/MarketAccess/utils/ShellInterface.java
@@ -1,170 +1,33 @@
 package ru.org.amip.MarketAccess.utils;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-import ru.org.amip.MarketAccess.view.StartUpView;
-
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.regex.Pattern;
 
 public class ShellInterface {
   private static String suBinary;
-  private static boolean suAvailable;
   private static boolean suChecked;
 
   private static final String XBIN_SU = "/system/xbin/su";
   private static final String BIN_SU = "/system/bin/su";
   private static final String TEST_COMMAND = "echo";
-  private static final String KILL_ALL = "killall";
   private static final String EXIT = "exit\n";
-  private static final String SETPREF = "setpref";
-  private static final Pattern PATTERN = Pattern.compile(" ");
-  private static final String SETOWN = "setown";
-
-  public static void doExec(String[] commands, boolean suNeeded, Handler handler) {
-    Process process = null;
-    DataOutputStream os = null;
-    DataInputStream osRes = null;
-    Message msg;
-    int i = 0;
-
-    try {
-      if (suNeeded && isSuAvailable()) {
-        process = Runtime.getRuntime().exec(suBinary);
-      } else {
-        process = Runtime.getRuntime().exec("sh");
-      }
-
-      os = new DataOutputStream(process.getOutputStream());
-      sinkProcessOutput(process);
-
-      for (String single : commands) {
-        Log.i(StartUpView.MARKET_ACCESS, single);
-        if (single.startsWith(KILL_ALL)) {
-          // special treatment for killall command, use java to kill the process
-          handleKill(single);
-        } else if (single.startsWith(SETPREF)) {
-          // setting preferences requires some context, get one from AppManager
-          handlePref(single, AppManager.getInstance());
-        } else if (single.startsWith(SETOWN)) {
-          handleOwn(single, AppManager.getInstance());
-        } else {
-          os.writeBytes(single + '\n');
-          os.flush();
-        }
-        msg = Message.obtain();
-        i++;
-        msg.arg1 = i;
-        msg.arg2 = -1; // 0 will dismiss the progress bar
-        handler.sendMessage(msg);
-        Thread.sleep(200);
-      }
-      os.writeBytes(EXIT);
-      os.flush();
-      process.waitFor();
-      msg = Message.obtain();
-      msg.arg1 = 0;
-      msg.arg2 = 0;
-      handler.sendMessage(msg);
-    } catch (Exception e) {
-      e.printStackTrace();
-      msg = Message.obtain();
-      msg.arg1 = 1;
-      msg.arg2 = 0;
-      handler.sendMessage(msg);
-    } finally {
-      try {
-        if (os != null) {
-          os.close();
-        }
-        if (osRes != null) {
-          osRes.close();
-        }
-        if (process != null) {
-          process.destroy();
-        }
-      } catch (Exception ignored) {}
-    }
-  }
-
-  private static void handleOwn(String single, Context ctx) {
-    final String[] parts = PATTERN.split(single, 3);
-    try {
-      final int uid = ctx.getPackageManager().getApplicationInfo(parts[1], 0).uid;
-      Log.i(StartUpView.MARKET_ACCESS, "setting owner: " + uid);
-      runCommand("chown " + uid + '.' + uid + ' ' + parts[2]);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private static void handlePref(String single, Context ctx) {
-    final String[] parts = PATTERN.split(single, 6);
-    try {
-      Context app = ctx.createPackageContext(parts[1], 0);
-      final SharedPreferences preferences =
-        app.getSharedPreferences(parts[2], Context.MODE_WORLD_READABLE + Context.MODE_WORLD_WRITEABLE);
-      // TODO add support for other types if you want it to be universal
-      // only change if set to false, don't add new preference or overwrite if already true
-      if (parts[3].equals("boolean") && !preferences.getBoolean(parts[4], true)) {
-        final SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean(parts[4], Boolean.parseBoolean(parts[5]));
-        editor.commit();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private static void handleKill(String single) throws IOException {
-    if (single.indexOf(' ') > 0) {
-      final String app = single.substring(single.indexOf(' ') + 1);
-      final AppManager am = AppManager.getInstance();
-      int count = 0;
-      while (am.isRunning(app)) {
-        count++;
-        am.kill(app);
-        if (am.isRunning(app)) {
-          Log.w(StartUpView.MARKET_ACCESS, "Failed to kill " + app);
-          try {
-            Thread.sleep(200);
-          } catch (InterruptedException ignored) {
-            break;
-          }
-        } else {
-          break;
-        }
-        if (count >= 5) {
-          Log.e(StartUpView.MARKET_ACCESS, "Failed to kill " + app + " 5 times, aborting");
-          throw new IOException("Can't kill app: " + app);
-        }
-      }
-    }
-  }
 
   public static synchronized boolean isSuAvailable() {
     if (!suChecked) {
       checkSu();
       suChecked = true;
     }
-    return suAvailable;
+    return suBinary != null;
   }
 
   private static boolean checkSu() {
-    suAvailable = true;
     suBinary = XBIN_SU;
-    if (runCommand(TEST_COMMAND)) return suAvailable;
+    if (runCommand(TEST_COMMAND)) return true;
     suBinary = BIN_SU;
-    if (runCommand(TEST_COMMAND)) return suAvailable;
-    suBinary = "";
-    suAvailable = false;
-    return suAvailable;
+    if (runCommand(TEST_COMMAND)) return true;
+    suBinary = null;
+    return false;
   }
 
   public static boolean runCommand(String command) {
