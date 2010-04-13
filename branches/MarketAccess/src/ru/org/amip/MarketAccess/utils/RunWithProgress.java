023ec378c1b70b1b0ef35595e00ece57b4d2b42e
sbaranov <sbaranov@a70d69da-39a2-11de-8ef0-0f53c1b7e458>
2010-04-13 11:44:53 +0000

    refactor process execution so that each command runs in its own shell, now we don't rely on sleep to ensure that shell command is complete before executing java command depending on the result of the shell command

--- branches/MarketAccess/src/ru/org/amip/MarketAccess/utils/RunWithProgress.java
+++ branches/MarketAccess/src/ru/org/amip/MarketAccess/utils/RunWithProgress.java
@@ -4,10 +4,16 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.widget.Toast;
 import ru.org.amip.MarketAccess.R;
+import ru.org.amip.MarketAccess.view.StartUpView;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * Date: Mar 24, 2010
@@ -22,6 +28,12 @@ public class RunWithProgress implements Runnable {
   private final String message;
   private CompleteListener completeListener;
   private boolean silent;
+
+  private static final Pattern PATTERN = Pattern.compile(" ");
+
+  private static final String KILL_ALL = "killall";
+  private static final String SETPREF = "setpref";
+  private static final String SETOWN = "setown";
 
   private static final String[] COMMANDS = new String[]{
     "setprop gsm.sim.operator.numeric",
@@ -116,6 +128,98 @@ public class RunWithProgress implements Runnable {
       handler.sendMessage(msg);
       return;
     }
-    ShellInterface.doExec(writePropCommand, true, handler);
+    doExec(writePropCommand, handler);
+  }
+
+  private static void doExec(String[] commands, Handler handler) {
+    Message msg;
+    int i = 0;
+
+    try {
+      for (String cmd : commands) {
+        Log.i(StartUpView.MARKET_ACCESS, cmd);
+        if (cmd.startsWith(KILL_ALL)) {
+          // special treatment for killall command, use java to kill the process
+          handleKill(cmd);
+        } else if (cmd.startsWith(SETPREF)) {
+          // setting preferences requires some context, get one from AppManager
+          handlePref(cmd, AppManager.getInstance());
+        } else if (cmd.startsWith(SETOWN)) {
+          handleOwn(cmd, AppManager.getInstance());
+        } else {
+          if (!ShellInterface.runCommand(cmd)) throw new IOException("Shell command failed: " + cmd);
+        }
+        msg = Message.obtain();
+        i++;
+        msg.arg1 = i;
+        msg.arg2 = -1; // 0 will dismiss the progress bar
+        handler.sendMessage(msg);
+      }
+      msg = Message.obtain();
+      msg.arg1 = 0;
+      msg.arg2 = 0;
+      handler.sendMessage(msg);
+    } catch (Exception e) {
+      e.printStackTrace();
+      msg = Message.obtain();
+      msg.arg1 = 1;
+      msg.arg2 = 0;
+      handler.sendMessage(msg);
+    }
+  }
+
+  private static void handleOwn(String single, Context ctx) {
+    final String[] parts = PATTERN.split(single, 3);
+    try {
+      final int uid = ctx.getPackageManager().getApplicationInfo(parts[1], 0).uid;
+      Log.i(StartUpView.MARKET_ACCESS, "setting owner: " + uid);
+      ShellInterface.runCommand("chown " + uid + '.' + uid + ' ' + parts[2]);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void handlePref(String single, Context ctx) {
+    final String[] parts = PATTERN.split(single, 6);
+    try {
+      Context app = ctx.createPackageContext(parts[1], 0);
+      final SharedPreferences preferences =
+        app.getSharedPreferences(parts[2], Context.MODE_WORLD_READABLE + Context.MODE_WORLD_WRITEABLE);
+      // TODO add support for other types if you want it to be universal
+      // only change if set to false, don't add new preference or overwrite if already true
+      if (parts[3].equals("boolean") && !preferences.getBoolean(parts[4], true)) {
+        final SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(parts[4], Boolean.parseBoolean(parts[5]));
+        editor.commit();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void handleKill(String single) throws IOException {
+    if (single.indexOf(' ') > 0) {
+      final String app = single.substring(single.indexOf(' ') + 1);
+      final AppManager am = AppManager.getInstance();
+      int count = 0;
+      while (am.isRunning(app)) {
+        count++;
+        am.kill(app);
+        if (am.isRunning(app)) {
+          Log.w(StartUpView.MARKET_ACCESS, "Failed to kill " + app);
+          try {
+            Thread.sleep(200);
+          } catch (InterruptedException ignored) {
+            break;
+          }
+        } else {
+          break;
+        }
+        if (count >= 5) {
+          Log.e(StartUpView.MARKET_ACCESS, "Failed to kill " + app + " 5 times, aborting");
+          throw new IOException("Can't kill app: " + app);
+        }
+      }
+    }
   }
 }
